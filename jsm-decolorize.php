23abef851924c41906e9d3904c8f8f280d281c8d
JS Morisset <jsm@surniaulula.com>
2022-01-11 10:33:33 -0800

    v1.0.0

---
+++ jsm-decolorize.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Plugin Name: JSM Decolorize
+ * Text Domain: jsm-decolorize
+ * Domain Path: /languages
+ * Plugin URI: https://surniaulula.com/extend/plugins/jsm-decolorize/
+ * Assets URI: https://jsmoriss.github.io/jsm-decolorize/assets/
+ * Author: JS Morisset
+ * Author URI: https://surniaulula.com/
+ * License: GPLv3
+ * License URI: https://www.gnu.org/licenses/gpl.txt
+ * Description: A simple plugin to decolorize the colorful admin menu icons added by some plugins.
+ * Requires PHP: 7.2
+ * Requires At Least: 5.2
+ * Tested Up To: 5.8.3
+ * Version: 1.0.0
+ *
+ * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
+ *
+ *      {major}         Major structural code changes / re-writes or incompatible API changes.
+ *      {minor}         New functionality was added or improved in a backwards-compatible manner.
+ *      {bugfix}        Backwards-compatible bug fixes or small improvements.
+ *      {stage}.{level} Pre-production release: dev < a (alpha) < b (beta) < rc (release candidate).
+ *
+ * Copyright 2017-2022 Jean-Sebastien Morisset (https://surniaulula.com/)
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+
+	die( 'These aren\'t the droids you\'re looking for.' );
+}
+
+if ( ! class_exists( 'JsmDecolorize' ) ) {
+
+	class JsmDecolorize {
+
+		private static $instance = null;	// JsmDecolorize class object.
+
+		public function __construct() {
+
+			add_action( 'admin_enqueue_scripts', array( __CLASS__, 'admin_menu_inline_style' ) );
+		}
+
+		public static function &get_instance() {
+
+			if ( null === self::$instance ) {
+
+				self::$instance = new self;
+			}
+
+			return self::$instance;
+		}
+
+		public static function admin_menu_inline_style() {
+
+			$custom_css = '#adminmenu li.menu-top a img { -webkit-filter:grayscale(100%); filter:grayscale(100%); }';
+
+			wp_add_inline_style( 'admin-menu', $custom_css );
+		}
+	}
+
+	JsmDecolorize::get_instance();
+}
---
+++ readme.txt
@@ -0,0 +1,65 @@
+=== JSM Decolorize ===
+Plugin Name: JSM Decolorize
+Plugin Slug: jsm-decolorize
+Text Domain: jsm-decolorize
+Domain Path: /languages
+License: GPLv3
+License URI: https://www.gnu.org/licenses/gpl.txt
+Assets URI: https://jsmoriss.github.io/jsm-decolorize/assets/
+Tags: plugin, icon, color, menu, admin
+Contributors: jsmoriss
+Requires PHP: 7.2
+Requires At Least: 5.2
+Tested Up To: 5.8.3
+Stable Tag: 1.0.0
+
+A simple plugin to decolorize the colorful admin menu icons added by some plugins.
+
+== Description ==
+
+A simple plugin to decolorize the colorful admin menu icons added by some plugins like NextGEN Gallery, Perfect Images, Wordfence Security, and many more.
+
+== Installation ==
+
+== Frequently Asked Questions ==
+
+== Screenshots ==
+
+== Changelog ==
+
+<h3 class="top">Version Numbering</h3>
+
+Version components: `{major}.{minor}.{bugfix}[-{stage}.{level}]`
+
+* {major} = Major structural code changes / re-writes or incompatible API changes.
+* {minor} = New functionality was added or improved in a backwards-compatible manner.
+* {bugfix} = Backwards-compatible bug fixes or small improvements.
+* {stage}.{level} = Pre-production release: dev < a (alpha) < b (beta) < rc (release candidate).
+
+<h3>Repositories</h3>
+
+* [GitHub](https://jsmoriss.github.io/jsm-decolorize/)
+* [WordPress.org](https://plugins.trac.wordpress.org/browser/jsm-decolorize/)
+
+<h3>Changelog / Release Notes</h3>
+
+**Version 1.0.0 (2022/01/11)**
+
+* **New Features**
+	* Initial release.
+* **Improvements**
+	* None.
+* **Bugfixes**
+	* None.
+* **Developer Notes**
+	* None.
+* **Requires At Least**
+	* PHP v7.0.
+	* WordPress v4.5.
+
+== Upgrade Notice ==
+
+= 1.0.0 =
+
+(2022/01/11) Initial release.
+
