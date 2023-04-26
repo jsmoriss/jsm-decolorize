5883aa2742534eb3b410d01d0697e530d6606e5a
JS Morisset <jsm@surniaulula.com>
2023-04-26 06:19:04 -0700

    v1.0.0

--- jsm-decolorize.php
+++ jsm-decolorize.php
@@ -10,7 +10,7 @@
  * License: GPLv3
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: A simple plugin to decolorize the colorful admin menu icons added by some plugins.
- * Requires PHP: 7.2
+ * Requires PHP: 7.2.5
  * Requires At Least: 5.5
  * Tested Up To: 6.2.0
  * Version: 1.0.0
