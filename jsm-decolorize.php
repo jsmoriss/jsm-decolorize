fecbe91efe59094c8df96babac2631a42f6ceb26
JS Morisset <jsm@surniaulula.com>
2023-05-16 10:12:05 -0700

    v1.0.0

--- jsm-decolorize.php
+++ jsm-decolorize.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Plugin Name: JSM's Decolorize Menu Icons
+ * Plugin Name: JSM Decolorize Menu Icons
  * Text Domain: jsm-decolorize
  * Domain Path: /languages
  * Plugin URI: https://surniaulula.com/extend/plugins/jsm-decolorize/
