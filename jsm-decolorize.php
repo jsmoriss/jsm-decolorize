dd4763c87c97895d8f81118f33ef3ca0a0120e70
jsmoriss <jsm@surniaulula.com>
2025-07-15 20:28:26 +0000

    v1.0.0

--- jsm-decolorize.php
+++ jsm-decolorize.php
@@ -12,7 +12,7 @@
  * Description: A simple plugin to decolorize the colorful admin menu icons added by some plugins.
  * Requires PHP: 7.4.33
  * Requires At Least: 5.9
- * Tested Up To: 6.8.1
+ * Tested Up To: 6.8.2
  * Version: 1.0.0
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
